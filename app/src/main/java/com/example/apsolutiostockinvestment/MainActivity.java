ad1b0d24867347caa5ba476b7cbfcdcfd18b5df6
Tirtha Chakraborty <maman.tirtha@gmail.com>
2020-04-11 12:29:42 +0530

    realm loaded on button click

---
+++ app/src/main/java/com/example/apsolutiostockinvestment/MainActivity.java
@@ -0,0 +1,289 @@
+package com.example.apsolutiostockinvestment;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import io.realm.Realm;
+
+//import butterknife.OnCheckedChanged;
+//import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    RadioGroup bottomnav;
+    Context context;
+    private static  final int MY_PERMISSION_REQUEST_STORAGE=1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        context = this;
+
+        //set ActionBar
+        ActionBar actionBar = getSupportActionBar(); // or getActionBar();
+        getSupportActionBar().setTitle("Portfolio");
+        bottomnav = findViewById(R.id.radioGroup1);
+
+        navigate(bottomnav);
+        FloatingActionButton myFab = (FloatingActionButton) findViewById(R.id.fb_add);
+
+        add(myFab);
+
+
+
+    }
+    public void loadcsv(View view){
+        SharedPreferences sharedPreferences = getSharedPreferences("UserDetails", MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        int counter = sharedPreferences.getInt("counter", 0);
+//        float font_size = editor.getFloat("font_size",20.0f);
+        if (counter == 0) {
+
+
+            copyAsset();   //copy all asset contents to internal storage
+            loadRealm();   //load all internal storage contents to realm
+
+            editor.putInt("counter", ++counter);
+            editor.commit();
+            Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+    public void navigate(RadioGroup bottomnav){
+
+        bottomnav.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkid) {
+                Intent in;
+                switch (checkid) {
+                    case R.id.overview:
+                        //code here
+                        break;
+                    case R.id.stocks:
+                        in = new Intent(MainActivity.this, Test.class);
+                        startActivity(in);
+                        overridePendingTransition(0, 0);
+                        break;
+                    case R.id.mf:
+                        //code here
+                        break;
+                    case R.id.ulip:
+                        //code here
+                        break;
+
+                }
+
+            }
+        });
+
+    }
+
+    public void copyAsset() {
+        AssetManager assetManager = getAssets();
+        String[] files = null;
+        try {
+            files = assetManager.list("");
+        } catch (IOException e) {
+            //Log.e("tag", "Failed to get asset file list.", e);
+        }
+        for(String filename : files) {
+            InputStream in = null;
+            OutputStream out = null;
+            try {
+                in = assetManager.open(filename);
+                File outFile = new File(getExternalFilesDir(null), filename);
+                out = new FileOutputStream(outFile);
+                copyFile(in, out);
+                in.close();
+                in = null;
+                out.flush();
+                out.close();
+                out = null;
+            } catch(IOException e) {
+               // Log.e("tag", "Failed to copy asset file: " + filename, e);
+            }
+        }
+    }
+        private void copyFile(InputStream in, OutputStream out) throws IOException {
+            byte[] buffer = new byte[1024];
+            int read;
+            while((read = in.read(buffer)) != -1){
+                out.write(buffer, 0, read);
+            }
+        }
+
+
+    public void add(FloatingActionButton myFab){
+
+
+        myFab.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                // custom dialog
+                final Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.popup_addaccount);
+                ImageButton dialogButton = (ImageButton) dialog.findViewById(R.id.closebtn);
+                // if button is clicked, close the custom dialog
+                dialogButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                        Toast.makeText(getApplicationContext(),"Dismissed..!!",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dialog.show();
+                Window window = dialog.getWindow();
+                window.setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            }
+        });
+
+    }
+
+
+    public void loadRealm() {
+
+        try {
+
+            File csvNfile=new File(getExternalFilesDir(null),"/All_NSE_Scrips.csv");
+            final CSVParser parser =
+                    new CSVParserBuilder()
+                            .withSeparator(',')
+                            .withIgnoreQuotations(true)
+                            .build();
+            CSVReader reader_N = new CSVReaderBuilder(new FileReader(csvNfile)).withCSVParser(parser).build();
+
+            // read line by line for NSE
+
+
+            String[] record = null;
+
+            while ((record = reader_N.readNext()) != null) {
+                createStockItem("N", record);
+            }
+
+            reader_N.close();
+
+            // read line by line for BSE
+
+
+
+            File csvBfile = new File(getExternalFilesDir(null), "All_BSE_Scrips.csv");
+            CSVReader reader_B = new CSVReaderBuilder(new FileReader(csvBfile)).withCSVParser(parser).build();
+
+            String[] record1 = null;
+
+            while ((record1 = reader_N.readNext()) != null) {
+                createStockItem("B", record1);
+            }
+
+            reader_B.close();
+
+            Toast.makeText(this, "Read", Toast.LENGTH_SHORT).show();
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "The specified file was not found", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+
+    public void createStockItem(String t, String[] record){
+
+        Realm realm=Realm.getDefaultInstance();
+        realm.beginTransaction();
+        try{
+            Number currentIdNum=realm.where(Stocks.class).max("stock_id");
+            int nextId = (currentIdNum == null) ? 1 : currentIdNum.intValue() + 1;
+            Stocks stock=realm.createObject(Stocks.class,nextId);
+            stock.setType(t);
+
+            if(t.equals("B")) {
+                //stock.setStock_id(nextId);
+                stock.setSecurity_code(Integer.parseInt(record[0]));
+                stock.setSecurity_id(record[2]);
+                stock.setSecurity_name(record[3]);
+                stock.setStatus(record[4]);
+                stock.setGroup(record[5]);
+                stock.setFace_value(Integer.parseInt(record[6]));
+                stock.setIsin(record[7]);
+                stock.setIndustry(record[8]);
+                stock.setInstrument(record[9]);
+            }
+            if(t.equals("N")){
+                // stock.setStock_id(nextId);
+                stock.setSymbol(record[0]);
+                stock.setCompany_ame(record[1]);
+                stock.setSeries(record[2]);
+                stock.setListing_date(record[3]);
+                stock.setPaid_up_value(Integer.parseInt(record[4]));
+                stock.setMarket_lot(Integer.parseInt(record[5]));
+                stock.setIsin(record[6]);
+                stock.setFace_value(Integer.parseInt(record[7]));
+
+            }
+//            SimpleDateFormat f=new SimpleDateFormat("dd/MM/yyyy");
+            // Date d=f.parse(fromDate.getText().toString());
+
+
+
+
+            realm.commitTransaction();
+            Toast.makeText(this, "Realm Loaded!!"+stock.getIsin(), Toast.LENGTH_SHORT).show();
+
+            // Intent intent=new Intent(this,Dashboard.class);
+            //startActivity(intent);
+
+        }
+        catch(Exception e){realm.cancelTransaction();
+            Toast.makeText(this, "realm failed", Toast.LENGTH_SHORT).show();}
+        finally {
+            realm.close();
+        }
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/apsolutiostockinvestment/Stocks.java
@@ -0,0 +1,169 @@
+package com.example.apsolutiostockinvestment;
+
+import java.sql.Time;
+import java.util.Date;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Stocks extends RealmObject {
+    @PrimaryKey
+    private int stock_id;
+    private String type;
+    private String symbol;
+    private String company_ame;
+    private String listing_date;
+    private String series;
+    private int paid_up_value;
+    private int market_lot;
+    private String isin;
+    private int face_value;
+    private  int security_code;
+    private  String status;
+    private String security_id;
+    private String security_name;
+    private String group;
+    private String industry;
+    private  String instrument;
+
+
+
+    public Stocks() {
+    }
+
+    public int getStock_id() {
+        return stock_id;
+    }
+
+    public void setStock_id(int stock_id) {
+        this.stock_id = stock_id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getCompany_ame() {
+        return company_ame;
+    }
+
+    public void setCompany_ame(String company_ame) {
+        this.company_ame = company_ame;
+    }
+
+    public String getListing_date() {
+        return listing_date;
+    }
+
+    public void setListing_date(String listing_date) {
+        this.listing_date = listing_date;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public int getPaid_up_value() {
+        return paid_up_value;
+    }
+
+    public void setPaid_up_value(int paid_up_value) {
+        this.paid_up_value = paid_up_value;
+    }
+
+    public int getMarket_lot() {
+        return market_lot;
+    }
+
+    public void setMarket_lot(int market_lot) {
+        this.market_lot = market_lot;
+    }
+
+    public String getIsin() {
+        return isin;
+    }
+
+    public void setIsin(String isin) {
+        this.isin = isin;
+    }
+
+    public int getFace_value() {
+        return face_value;
+    }
+
+    public void setFace_value(int face_value) {
+        this.face_value = face_value;
+    }
+
+    public int getSecurity_code() {
+        return security_code;
+    }
+
+    public void setSecurity_code(int security_code) {
+        this.security_code = security_code;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSecurity_id() {
+        return security_id;
+    }
+
+    public void setSecurity_id(String security_id) {
+        this.security_id = security_id;
+    }
+
+    public String getSecurity_name() {
+        return security_name;
+    }
+
+    public void setSecurity_name(String security_name) {
+        this.security_name = security_name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getInstrument() {
+        return instrument;
+    }
+
+    public void setInstrument(String instrument) {
+        this.instrument = instrument;
+    }
+}

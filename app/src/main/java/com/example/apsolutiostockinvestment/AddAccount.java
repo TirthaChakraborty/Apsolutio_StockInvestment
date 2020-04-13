d22f60af2fbed2c791c42f93f1bcdc173f6fc906
Tirtha Chakraborty <maman.tirtha@gmail.com>
2020-04-13 16:05:28 +0530

    Merge branch 'master' of C:\Users\Tirtha Chakraborty\AndroidStudioProjects\ApsolutioStockInvestment with conflicts.

--- app/src/main/java/com/example/apsolutiostockinvestment/AddAccount.java
+++ app/src/main/java/com/example/apsolutiostockinvestment/AddAccount.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -17,18 +16,10 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.FragmentTransaction;
-
-import java.util.Date;
-
-import io.realm.Realm;
 
 public class AddAccount extends DialogFragment {
     private ImageButton close;
     private AutoCompleteTextView compname;
-    private Button adddone;
-    private EditText units,price;
-    private TextView totalamount;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -42,51 +33,32 @@ public class AddAccount extends DialogFragment {
         });
         compname=view.findViewById(R.id.stockname);
         compname.setAdapter(new StockAutocompleteAdapter(getActivity()));
-        units=view.findViewById(R.id.unit);
-        price=view.findViewById(R.id.unitprice);
-        totalamount=view.findViewById(R.id.price);
-        adddone=view.findViewById(R.id.adddone);
-        adddone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Realm realm=Realm.getDefaultInstance();
-                Stocks s=realm.where(Stocks.class).equalTo("company_ame",compname.getText().toString())
-                        .or().equalTo("security_name",compname.getText().toString()).findFirst();
-                if(s!=null)
-                {realm.beginTransaction();
-                UserStock userStock=new UserStock();
-                Date d=new Date();
-                userStock.setId(d.toString());
-                userStock.setStockname(compname.getText().toString());
-                userStock.setDate(d);
-                userStock.setUnits(Long.parseLong(units.getText().toString()));
-                userStock.setPrice(Double.parseDouble(price.getText().toString()));
-                userStock.setTotalamount(Double.parseDouble(totalamount.getText().toString().replaceAll("Rs.","")));
-                realm.copyToRealm(userStock);
-                realm.commitTransaction();
-                DialogFragment successdialog=new AddSuccessAnimationDialog();
-                FragmentTransaction fragmentTransaction=getFragmentManager().beginTransaction();
-                successdialog.show(fragmentTransaction,null);
-            }
-            else
-                    Toast.makeText(getActivity(), "Stock not in list", Toast.LENGTH_LONG).show();}
-        });
 
 
         //check whether unit is whole number or not
 
-        //final TextView total=(TextView)view.findViewById(R.id.price);
-        //final EditText price_per_unit = (EditText) view.findViewById(R.id.unitprice);
-
-        units.addTextChangedListener(new TextWatcher() {
+        final TextView total=(TextView)view.findViewById(R.id.price);
+        final EditText price_per_unit = (EditText) view.findViewById(R.id.unitprice);
+        final EditText num_of_units = (EditText) view.findViewById(R.id.unit);
+        num_of_units.addTextChangedListener(new TextWatcher() {
 
             @Override
             public void afterTextChanged(Editable s) {
+                double unit;
                 String str=s.toString();
-                double unit=Double.parseDouble(str);
+                if(str.isEmpty()){
+                    unit=0.0;
+                }
+                else{ unit=Double.parseDouble(str);}
                 if(unit%1!=0){
-                    units.setError("No. of Units must be a whole number");
+                    num_of_units.setError("No. of Units must be a whole number");
 
+                }
+                else if(!(s.toString().isEmpty())){
+                    price_per_unit.setFocusableInTouchMode(true);
+                }
+                else{
+                    price_per_unit.setFocusableInTouchMode(false);
                 }
 
             }
@@ -97,41 +69,94 @@ public class AddAccount extends DialogFragment {
             @Override
             public void onTextChanged(CharSequence s, int start,
                                       int before, int count) {
+                if(!(s.toString().isEmpty())){
+                    price_per_unit.setFocusableInTouchMode(true);
+
+                }
+                else{
+                    price_per_unit.setFocusableInTouchMode(false);
+                }
 
 
 
             }
         });
 
+
+
         // to calculate the total amount
 
-        price.addTextChangedListener(new TextWatcher() {
+        price_per_unit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (num_of_units.getText().toString().isEmpty()) {
+                    num_of_units.setError("Enter no. of units");
+
+                }
+
+            }
+        });
+
+        price_per_unit.addTextChangedListener(new TextWatcher() {
 
             public void afterTextChanged(Editable s) {
-
-                if(units.getText().toString().isEmpty()){
-                    units.setError("Enter no. of units");
-                    price.setText("");
+                double rate,unit;
+                if(num_of_units.getText().toString().isEmpty()){
+                    unit=0.0;
                 }
                 else {
-                    double unit = Double.parseDouble(units.getText().toString());
-                    String s1 = s.toString();
-                    double rate = Double.parseDouble(s1);
-                    double total_price = unit * rate;
-
-                    totalamount.setText("Rs." + total_price);
+                    unit = Double.parseDouble(num_of_units.getText().toString());
                 }
+                String s1 = s.toString();
+                if(s1.isEmpty()){
+                    rate=0.0;
+                }
+                else {
+                    rate = Double.parseDouble(s1);
+                }
+                double total_price = unit * rate;
+
+                total.setText("Rs." + (Double.toString(total_price)));
+
 
             }
 
             public void beforeTextChanged(CharSequence s, int start,
                                           int count, int after) {
+
+                if (num_of_units.getText().toString().isEmpty()) {
+                    num_of_units.setError("Enter no. of units");
+//                    price_per_unit.setText(null);
+                }
             }
 
             public void onTextChanged(CharSequence s, int start,
                                       int before, int count) {
+                double rate,unit;
+                if(num_of_units.getText().toString().isEmpty()){
+                    unit=0.0;
+                }
+                else {
+                    unit = Double.parseDouble(num_of_units.getText().toString());
+                }
+                String s1 = s.toString();
+                if(s1.isEmpty()){
+                    rate=0.0;
+                }
+                else {
+                    rate = Double.parseDouble(s1);
+                }
+                double total_price = unit * rate;
+
+                total.setText("Rs." + (Double.toString(total_price)));
+
+
 
             }
+
+
+
         });
 
         return view;
